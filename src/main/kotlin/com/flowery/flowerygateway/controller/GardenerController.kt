73fdce8573fc97dfbe60b0efdf8c3a1a7d776c21
jungry <tp0312@hanyang.ac.kr>
2025-01-21 17:29:43 +0900

    Controller 수정

--- src/main/kotlin/com/flowery/flowerygateway/controller/GardenerController.kt
+++ src/main/kotlin/com/flowery/flowerygateway/controller/GardenerController.kt
@@ -2,23 +2,20 @@ package com.flowery.flowerygateway.controller
 
 import com.flowery.flowerygateway.dto.LoginRequest
 import com.flowery.flowerygateway.dto.SignupRequest
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/")
 class GardenerController {
 
-    @PostMapping("/signup")
+    @PutMapping("/user")
     fun signUp(@RequestBody signUpRequest: SignupRequest): String {
 
         // GardenerService.createGardener(GardenerRepository)
         return ("회원가입 완료")
     }
 
-    @PostMapping("/login")
+    @PostMapping("session/new")
     fun logIn(@RequestBody loginRequest: LoginRequest): String {
 
         return ("로그인 완료")
